ffc443ec99cbb23769801b9233200b99475bba41
BulinskiD <dawidbulinski132@gmail.com>
2019-01-21 00:32:59 +0100

    Correct version

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
-﻿<?php 
+<?php 
 get_header();
 ?>
 
 
         <!--Content Block-->
         <section id="content" class="row">
-<<<<<<< HEAD
-            <div class="col-lg-4 col-0"></div>    
-            <div class="col-lg-8 col-12">
-              <h2 hidden="hidden">Bezstresowe prawo jazdy w Krakowie</h2>
-=======
             <div class="col-lg-4 col-0" style="z-index:-1"></div>    
             <div class="col-lg-8 col-12">
              <div class="eds-animate animated zoomIn" data-eds-entry-animation="zoomIn" data-eds-entry-delay="0" data-eds-entry-duration="0.5" data-eds-entry-timing="linear" data-eds-exit-animation="" data-eds-exit-delay="" data-eds-exit-duration="" data-eds-exit-timing="" data-eds-repeat-count="1" data-eds-keep="yes" data-eds-animate-on="load" data-eds-scroll-offset="" style="animation-duration: 0.5s; animation-delay: 0s; animation-timing-function: linear; animation-iteration-count: 1;">
               <h1 class="front-page_header">Prawo jazdy Kraków - Juvex</h1>
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators d-lg-flex d-none">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -28,11 +18,7 @@ get_header();
   <div class="carousel-inner">
 
     <div class="carousel-item active">
-<<<<<<< HEAD
-                      <a href="?page_id=77" style="text-decoration: none;">
-=======
                       <a href="/o-nas/kurs-kat-b/" style="text-decoration: none;">
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
                 <div id="post1">
                     <div class="half info text-center"> NAJBLIŻSZY KURS KAT. B ROZPOCZYNAMY 
                         <?php
@@ -44,49 +30,27 @@ get_header();
 
                     <div class="half picture1"></div>
                 </div>
-<<<<<<< HEAD
-=======
 
 
 
 
                   <div class="d-table d-lg-none mobilecarousel">
-<div class="half info left">
-                Szkolimy od 1994
-                <br />
-                <br />
-                profesjonalnie <br />
-                bezstresowo <br />
-                bezpiecznie <br />
-</div>
+                    <div class="half info left">
+                     Szkolimy od 1994
+                        <br />
+                        <br />
+                    profesjonalnie <br />
+                    bezstresowo <br />
+                    bezpiecznie <br />
+                    </div>
 
 <div class="half info" style="margin-left:15px;">Dowiedz się <br /> więcej</div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
-            </a>
+</a>
     </div>
 
     <div class="carousel-item">
-<<<<<<< HEAD
-            <a href="?page_id=77" style="text-decoration: none;">
-=======
             <a href="/cennik-krakow-juvex/" style="text-decoration: none;">
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
                 <div id="post2">
                     <div class="half info text-center"> 
                        SPRAWDŹ NASZE CENY
@@ -95,8 +59,6 @@ get_header();
                          </div>
                     <div class="half picture2"></div>
                 </div>
-<<<<<<< HEAD
-=======
 
 
                        <div class="d-table d-lg-none mobilecarousel">
@@ -108,31 +70,13 @@ get_header();
                 bezstresowo <br />
                 bezpiecznie <br />
 </div>
-
 <div class="half info" style="margin-left:15px;">Dowiedz się <br /> więcej</div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
-            </a>
+</a>
     </div>
 
     <div class="carousel-item ">
-<<<<<<< HEAD
-                      <a href="?page_id=77" style="text-decoration: none;">
-=======
                       <a href="/samochody/" style="text-decoration: none;">
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
                 <div id="post3">
                     <div class="half info text-center">
                         OBEJRZYJ NASZE SAMOCHODY
@@ -142,8 +86,6 @@ get_header();
 
                     <div class="half picture3"></div>
                 </div>
-<<<<<<< HEAD
-=======
 
 
                        <div class="d-table d-lg-none mobilecarousel">
@@ -157,17 +99,9 @@ get_header();
 </div>
 
 <div class="half info" style="margin-left:15px;">Dowiedz się <br /> więcej</div>
+</div></a>
 </div>
-
-
-
-
-                
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
-            </a>
-    </div>
-
-  </div>
+</div>
 
 
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -183,31 +117,7 @@ get_header();
 
 
 </div> <!-- end carousel -->
-
-
-<<<<<<< HEAD
-<div class="d-table d-lg-none mobilecarousel">
-<div class="half info left">
-                Szkolimy od 1994
-                <br />
-                <br />
-                profesjonalnie <br />
-                bezstresowo <br />
-                bezpiecznie <br />
 </div>
-
-<div class="half info" style="margin-left:15px;">Dowiedz się <br /> więcej</div></a>
-</div>
-
-
-
-
-=======
-
-
-
-</div>
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
 </div>    
 </section>
 </div>
@@ -215,91 +125,5 @@ get_header();
 
 <?php 
 get_footer();
-<<<<<<< HEAD
+
 ?>
-=======
-?>
-=======
-﻿<!DOCTYPE html>
-
-<html lang="pl" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/layout.css">
-    <meta charset="utf-8" />
-    <title>Juvex</title>
-</head>
-<body>
-
-
-
-    <header id="logo">
-        <svg viewBox="0 0 500 410">
-            <rect width="360" height="260" style="fill-opacity:0;stroke-width:10;stroke:rgb(23,161,165)" />
-            <rect x="280" y="180" width="120" height="220" style="fill-opacity:0;stroke-width:10;stroke:rgb(23,161,165)" />
-        </svg>
-    </header>
-
-
-    <div class="container-fluid">
-
-        <!--Navbar-->
-        <div class="row">
-            <div class="col-lg-4"></div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-white col-lg-8" id="navbar">
-                    <a class="navbar-brand" href="#"></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="text-center nav-item"><div class="tabtext">Strona Główna</div></li>
-                            <li class="text-center nav-item"><div class="tabtext">O nas</div></li>
-                            <li class="text-center nav-item"><div class="tabtext">Cennik</div></li>
-                            <li class="text-center nav-item"><div class="tabtext">Samochody</div></li>
-                            <li class="text-center nav-item"><div class="tabtext">Kontakt</div></li>
-                        </ul>
-                    </div>
-            </nav>
-        </div>
-
-        <!--Szkolimy-->
-        <aside id="teaching">
-            <span class="teachinginside">
-                Szkolimy od 1994
-                <br />
-                <br />
-                profesjonalnie
-                bezstresowo
-                bezpiecznie
-
-            </span>
-        </aside>
-
-
-
-        <!--Content Block-->
-        <section id="content" class="row">
-            <div class="col-lg-4"></div>
-            <div class="col-lg-8">
-                <div id="post">
-                    <div class="half info text-center"> NAJBLIŻSZY KURS ROZPOCZYNAMY 25.10.1929r. godz. 15</div>
-                    <div class="half picture"></div>
-                </div>
-            </div>
-
-        </section>
-
-    </div>
-
-
-    <footer id="foot"><span class="footerinfo">Projekt graficzny: Eliza Kuciakowska, Kodowanie: Dawid Buliński, Powered by Wordpress</span></footer>
-
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
->>>>>>> 3b0b5cabf17876c897f37545087c0c7f7c279427
->>>>>>> ffd6dd803652445b2a9212a9ecae47e8b06ba2d0
